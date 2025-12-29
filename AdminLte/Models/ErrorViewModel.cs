99b170a05a8f90d0bd4b33b1444f82437c49d4e6
mu2khomgrich <mewszone@hotmail.com>
2025-12-29 08:45:35 +0700

    initial Admin template c#

---
+++ AdminLte/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminLte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
